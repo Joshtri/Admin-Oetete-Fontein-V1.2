7fcf516c8b71abc43d689fed231164ca97f9aa85
Joshtri <stuffofyos1516@gmail.com>
2022-10-13 09:48:28 +0800

    backend full

---
+++ controller/mainadminView.js
@@ -0,0 +1,21 @@
+var express = require("express");
+const mysql = require("mysql");
+
+// Connection Pool
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+const pool = mysql.createPool({
+  connectionLimit: 100,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+
+
