b3f3f64322cfbdf25ec2e601f0bc7ee689541f58
Joshtri <stuffofyos1516@gmail.com>
2022-10-17 11:55:15 +0800

    terbaru

--- routes/index.js
+++ routes/index.js
@@ -43,15 +43,24 @@ router.get("/main-admin", function (req, res, next) {
               connection.query("SELECT COUNT(id_kematian) AS TotalMati FROM kematian", (err, rows6) => {
                   //when done with the connection, release it.
                 connection.query("SELECT COUNT(id_lahir) AS TotalLahir FROM kelahiran", (err, rows7) => {
-
-                      connection.release();
+                  connection.query("SELECT COUNT(id_Usaha) AS Tmikro from umkm WHERE omset ='Omset <= Rp.300 Juta' AND kekayaan_bersih_usaha = 'KBU <= Rp.50 Juta' OR omset ='Omset <= Rp.300 Juta' AND kekayaan_bersih_usaha = 'Rp.50 Juta < KBU <= Rp.500 Juta' OR omset = 'Rp.300 Juta < Omset <= Rp.2,5 Milliar' AND kekayaan_bersih_usaha = 'KBU <= Rp.50 Juta'", (err, rows8) => {
+                    //when done with the connection, release it.
+                    connection.query("SELECT COUNT(id_Usaha) AS Tmenengah from umkm WHERE omset ='Rp.2,5 Milliar < Omset <= Rp.50 Milliar' AND kekayaan_bersih_usaha = 'Rp.500 Juta < KBU <= Rp.10 Milliar'", (err, rows9) => {
+                        //when done with the connection, release it.
+                        connection.query("SELECT COUNT(id_Usaha) AS Tkecil from umkm WHERE omset ='Rp.300 Juta < Omset <= Rp.2,5 Milliar' AND kekayaan_bersih_usaha = 'KBU <= Rp.500 Juta' OR omset ='Rp.300 Juta < Omset <= Rp.2,5 Milliar' AND kekayaan_bersih_usaha = 'Rp.500 Juta < KBU <= Rp.10 Milliar' OR omset = 'Rp.2,5 Milliar < Omset <= Rp.50 Milliar' AND kekayaan_bersih_usaha = 'Rp.50 Juta < KBU <= Rp.500 Juta'", (err, rows10) => {
+                            //when done with the connection, release it.
+                            connection.release();
             
                       if (!err) { 
-                        res.render("main-admin", { rows1, rows2, rows3, rows4, rows5, rows6, rows7});
+                        res.render("main-admin", { rows1, rows2, rows3, rows4, rows5, rows6, rows7, rows8, rows9, rows10});
                       } else {
                         console.log(err);
                       }
-                      console.log("The data from user table: \n",rows1, rows2, rows3,rows4,rows5, rows6, rows7);
+                      console.log("The data from user table: \n",rows1, rows2, rows3,rows4,rows5, rows6, rows7,rows8, rows9, rows10);
+
+                    });
+                  });
+                }); 
                 });
               });
             }); 
